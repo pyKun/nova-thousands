47533b9b3eb64479a9cc3827a07cd86b2c336591
Kun Huang <gareth@openstacker.org>
2014-12-15 12:15:50 +0800

    add test utils and init test module

---
+++ tests/unit/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# Author: Kun Huang <academicgareth@gmail.com>
+
+import unittest
+
+class TestCmdMain(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_parse_config(self):
+        pass # TODO
